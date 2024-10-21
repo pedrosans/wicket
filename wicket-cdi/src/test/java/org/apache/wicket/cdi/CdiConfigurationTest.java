c95e5d3347417d6dfea57a3c72c6c9bbb34ef609
Pedro Santos <pedrosans@gmail.com>
2024-10-21 11:49:45 -0300

    WICKET-7125 setBeanManager method test

--- wicket-cdi/src/test/java/org/apache/wicket/cdi/CdiConfigurationTest.java
+++ wicket-cdi/src/test/java/org/apache/wicket/cdi/CdiConfigurationTest.java
@@ -19,25 +19,44 @@ package org.apache.wicket.cdi;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.apache.wicket.Application;
+import org.apache.wicket.cdi.testapp.AlternativeTestAppScope;
+import org.apache.wicket.cdi.testapp.ModelWithInjectedDependency;
 import org.apache.wicket.cdi.testapp.TestConversationPage;
 import org.apache.wicket.cdi.testapp.TestPage;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import io.github.cdiunit.ActivatedAlternatives;
+import jakarta.enterprise.inject.spi.BeanManager;
+import jakarta.inject.Inject;
+
 /**
  * @author jsarman
  */
-// FIXME Wicket 10. Re-enable once cdi-unit is adapted to jakarta.**
+// FIXME Wicket 11. Re-enable once the tests are moved to its own module
 @Disabled
+@ActivatedAlternatives(AlternativeTestAppScope.class)
 class CdiConfigurationTest extends WicketCdiTestCase
 {
+	@Inject
+	BeanManager beanManager;
+
 	@Test
 	void testApplicationScope()
 	{
 		configure(new CdiConfiguration());
 		tester.startPage(TestPage.class);
 		tester.assertLabel("appscope", "Test ok");
+	}
+
+	@Test
+	void testUsesCdiJUnitConfiguration()
+	{
+		configure(new CdiConfiguration().setBeanManager(beanManager));
+		tester.startPage(TestPage.class);
+		tester.assertLabel("appscope", "Alternative ok");
 	}
 
 	@Test
@@ -50,6 +69,26 @@ class CdiConfigurationTest extends WicketCdiTestCase
 			tester.assertCount(i);
 			tester.clickLink("increment");
 		}
+	}
+
+	@Test
+	void testNotConfigured()
+	{
+		assertThrows(IllegalStateException.class, () -> {
+			new ModelWithInjectedDependency();
+		});
+
+	}
+
+	@Test
+	void testAlreadyConfigured()
+	{
+		configure(new CdiConfiguration());
+
+		assertThrows(IllegalStateException.class, () -> {
+			CdiConfiguration.get(Application.get()).setBeanManager(beanManager);
+		});
+
 	}
 
 	@Test
